a8a9750bfbf8caac0c468d79d61633147fa838c6
mihaivisu <mihaivisuian@gmail.com>
2015-08-19 23:45:10 +0100

    add more minified libs and GUI js files to gitignore and install more tasks in the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,46 @@
-/**
- * Created by mihaivisuian on 19/08/15.
- */
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: [
+                    'frontend/static/frontend/generated/js/project_gui.js <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                    'frontend/static/frontend/generated/js/project_libs.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                ]
+            },
+            build: {
+                src: 'frontend/static/frontend/js/*.js',
+                dest: 'frontend/static/frontend/generated/js/project_libs.min.js'
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                files: {
+                    'frontend/static/frontend/generated/js/project_libs.min.js': [
+                        'node_modules/jquery/dist/jquery.min.js',
+                        'node_modules/bootstrap/dist/js/bootstrap.min.js'
+                    ],
+                    'frontend/static/frontend/generated/css/project_libs.min.css': [
+                        'node_modules/bootstrap/dist/css/bootstrap.min.css'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
