3e90777830b96329a15f1a8efa58f2e8bfb53031
mihaivisu <mihaivisuian@gmail.com>
2015-12-07 23:01:19 +0000

    modify grunt

--- gruntfile.js
+++ gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
             },
             styles: {
                 files: ['less/*.less'],
-                tasks: ['less']
+                tasks: ['less:gui']
             }
         },
         uglify: {
